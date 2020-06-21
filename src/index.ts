6546e369e828fbc19ada7a4f7dbecfbf4043139e
Henrik Ræder Clausen <henrik@localhost.localdomain>
2020-06-21 15:59:18 +0200

    Lille ændring

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ createConnection().then(async connection => {
     const users = await connection.manager.find(Lærer);
     console.log("Loaded users: ", users);
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+    console.log("Here you can setup and run express/koa/any framework.");
 
 }).catch(error => console.log(error));
